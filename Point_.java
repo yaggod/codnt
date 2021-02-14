e9c583b054fde10a3498452b6e8b1099b85c9d0d
yaggod <71946695+yaggod@users.noreply.github.com>
2021-02-15 01:38:40 +0200

    Add files via upload

---
+++ Point_.java
@@ -0,0 +1,14 @@
+
+public class Point_ extends java.awt.Point {
+
+	@Override
+	public String toString() {
+		return "x - " + this.x + "; y - " + this.y;
+		
+	}
+	
+	Point_(java.awt.Point ptn){
+		super(ptn);
+	}
+	
+}
