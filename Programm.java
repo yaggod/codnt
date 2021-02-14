b936a75c1e6cce16667d2487ba83081a124a1266
yaggod <71946695+yaggod@users.noreply.github.com>
2021-02-15 01:37:51 +0200

    Add files via upload

--- Programm.java
+++ Programm.java
@@ -4,14 +4,14 @@ import javax.swing.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 public class Programm
 {
-	private static ArrayList<Point> all = new ArrayList<Point>();
+	private static ArrayList<Point_> all = new ArrayList<Point_>();
 	private static Robot rb;
-	
+	private static JProgressBar bar;
+	private static JSpinner fld;
 	private Programm() { }
 	
 
@@ -20,14 +20,21 @@ public class Programm
 		JFrame win = new JFrame("bot");
 		win.setAlwaysOnTop(true);
 		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		win.setLayout(new FlowLayout());
-		win.setSize(300, 200);
+		win.setLayout(null);
+		win.setSize(275, 111);
 		win.setResizable(false);
-		JButton btn = new JButton("use it");
-		JButton addBtn = new JButton("add point");
+		
+		JButton btn = new JButton("use it"); btn.setBounds(5, 5, 80, 30);
+		JButton clr = new JButton("clear"); clr.setBounds(90, 5, 80, 30);
+		JButton addBtn = new JButton("add"); addBtn.setBounds(175,5,80,30);		
+		fld = new JSpinner(); fld.setBounds(5,40,165,30);
+		bar = new JProgressBar();  bar.setBounds(175,40,80,30); bar.setMaximum(37); bar.setValue(37);
+
 		btn.addActionListener(new ClickAct());
 		addBtn.addActionListener(new AddAct());
-		win.add(btn); win.add(addBtn);
+		clr.addActionListener(new ClearAct());
+		
+		win.add(btn); win.add(addBtn); win.add(clr); win.add(fld); win.add(bar);
 		win.setVisible(true);
 		
 	}
@@ -40,21 +47,24 @@ public class Programm
 		}
 		
 		public void run() {
-			int did = 0;
 			Point[] pnt;
 			pnt =  all.toArray(new Point[all.size()]);
 				for (Point single : pnt) {
 					rb.mouseMove(single.x, single.y);
 					rb.mousePress(InputEvent.BUTTON1_MASK);
-					rb.delay(50);
+					rb.delay(20);
 					rb.mouseRelease(InputEvent.BUTTON1_MASK);
-					rb.delay(135);
+					try {
+					rb.delay((Integer) fld.getValue());
+					} catch (IllegalArgumentException e){
+						e.printStackTrace();
+						rb.delay(100);
+					}
 				}
 				
 
 		}
 	}
-	 
 	 private static class AddAct implements java.awt.event.ActionListener, Runnable{
 
 			@Override
@@ -64,18 +74,26 @@ public class Programm
 
 			@Override
 			synchronized public void run() {
-				try {
-					Thread.sleep(300);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				for (int i=0;i<=37;i++) {
+					bar.setValue(i);
+					rb.delay(8);
 				}
-				all.add(MouseInfo.getPointerInfo().getLocation());
+				
+				
+			
+				Point_ p = new Point_(MouseInfo.getPointerInfo().getLocation());
+				all.add(p);
 
 			}
 			
 			
 		}
-	 
+	 private static class ClearAct implements java.awt.event.ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			all = new ArrayList<Point_>();
+		}
+	 }
 	 
 }
